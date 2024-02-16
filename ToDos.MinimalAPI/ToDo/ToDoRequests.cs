8c722d52b43cb26d365f69ab63012f8d630eb427
mikolaj <robakk16@gmail.com>
2024-02-16 13:31:59 +0100

    swagger descriptions

--- ToDos.MinimalAPI/ToDo/ToDoRequests.cs
+++ ToDos.MinimalAPI/ToDo/ToDoRequests.cs
@@ -4,11 +4,31 @@ public static class ToDoRequests
 {
     public static WebApplication RegisterEndpoints(this WebApplication app)
     {
-        app.MapGet("/todos", ToDoRequests.GetAll);
-        app.MapGet("/todos/{id}", ToDoRequests.GetById);
-        app.MapPost("/todos", ToDoRequests.Create);
-        app.MapPut("/todos/{id}", ToDoRequests.Update);
-        app.MapDelete("/todos/{id}", ToDoRequests.Delete);
+        app.MapGet("/todos", ToDoRequests.GetAll)
+            .Produces<List<ToDo>>()
+            .WithTags("To Dos");   
+        
+        app.MapGet("/todos/{id}", ToDoRequests.GetById)
+            .Produces<ToDo>()
+            .Produces(StatusCodes.Status404NotFound)
+            .WithTags("To Dos");
+
+        app.MapPost("/todos", ToDoRequests.Create)
+            .Produces<ToDo>(StatusCodes.Status201Created)
+            .Accepts<ToDo>("application/json")
+            .WithTags("To Dos");
+
+        app.MapPut("/todos/{id}", ToDoRequests.Update)
+            .Produces(StatusCodes.Status204NoContent)
+            .Produces(StatusCodes.Status404NotFound)
+            .Accepts<ToDo>("application/json")
+            .WithTags("To Dos");
+
+        app.MapDelete("/todos/{id}", ToDoRequests.Delete)
+            .Produces(StatusCodes.Status204NoContent)
+            .Produces(StatusCodes.Status404NotFound)
+            .WithTags("To Dos")
+            .ExcludeFromDescription();
 
         return app;
     }
